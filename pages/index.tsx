6f76c373628ab721c7cc95a4f15ebe2055124f6b
Michael Desormeaux <53596354+M-Desormeaux@users.noreply.github.com>
2022-06-11 15:20:06 -0500

    Feature/better blurb (#4)
    * better blurb
    
    * blurb 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,17 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-      <h1>Hello</h1>
+      <h1>👋 Howdy, I&apos;m Michael</h1>
+      <p>
+        Father of 2, husband, and enjoyer of many layers of Tech. Currently I am
+        a Frontend React Developer, with a passion for growth and
+        self-improvement. You can catch me around many developer communities,
+        involved in some way or another.
+      </p>
+      <p>
+        You can get find me on _Twitter_ or _LinkedIn, though I am far more
+        active in checking twitter.
+      </p>
     </div>
   );
 };
