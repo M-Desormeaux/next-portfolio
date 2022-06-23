a0c7da5061c7bbe38d4b4e77badc189b5ecef04f
Michael Desormeaux <53596354+M-Desormeaux@users.noreply.github.com>
2022-06-23 11:12:15 -0500

    Test/checking to see if staging updates (#8)
    * version change
    
    * blurb link updates
    
    * spacing fixes
    
    * improvements?
    
    * even better!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,21 @@
 import { Navbar } from "../components/Navbar/Navbar";
 
 const Home = () => {
+  const twitterLink = (
+    <a href="https://twitter.com/meliboi_" target="_blank" rel="noreferrer">
+      Twitter
+    </a>
+  );
+  const linkedInLink = (
+    <a
+      href="https://www.linkedin.com/in/m-desormeaux/"
+      target="_blank"
+      rel="noreferrer"
+    >
+      LinkedIn
+    </a>
+  );
+
   return (
     <div>
       <Navbar />
@@ -12,8 +27,7 @@ const Home = () => {
         involved in some way or another.
       </p>
       <p>
-        You can get find me on _Twitter_ or _LinkedIn, though I am far more
-        active in checking twitter.
+        You can get find me at {twitterLink} or {linkedInLink}.
       </p>
     </div>
   );
