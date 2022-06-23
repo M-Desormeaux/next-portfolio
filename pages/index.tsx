2ff5044d60b09cfa608bf14501bfe7c3a9aa8c12
Michael Desormeaux <mdesormeaux96@gmail.com>
2022-06-23 11:03:47 -0500

    improvements?

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
 import { Navbar } from "../components/Navbar/Navbar";
 
 const Home = () => {
+  const twitterLink = <a href="https://twitter.com/meliboi_">Twitter</a>;
+  const linkedInLink = (
+    <a href="https://www.linkedin.com/in/m-desormeaux/">LinkedIn</a>
+  );
+
   return (
     <div>
       <Navbar />
@@ -12,9 +17,7 @@ const Home = () => {
         involved in some way or another.
       </p>
       <p>
-        You can get find me on
-        <a href="https://twitter.com/meliboi_"> Twitter</a> or
-        <a href="https://www.linkedin.com/in/m-desormeaux/"> LinkedIn</a>.
+        You can get find me at {twitterLink} or {linkedInLink}.
       </p>
     </div>
   );
